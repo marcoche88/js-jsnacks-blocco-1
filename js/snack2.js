575403d4d8e02b7fe1a08f833837951826e6a55d
marcoche88 <marco.che@hotmail.it>
2021-07-07 15:04:10 +0200

    boilerplate + style + scripts

---
+++ js/snack2.js
@@ -0,0 +1,4 @@
+/*
+SNACK 2: Chiedi un numero all'utente, se è pari stampa il numero,
+se è dispari stampa il numero successivo.
+*/
---
+++ js/snack3.js
@@ -0,0 +1,4 @@
+/*
+SNACK 3: Generatore di “nomi cognomi” casuali: prendendo una lista di nomi
+e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+*/
